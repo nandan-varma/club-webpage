3704ded860801d5b48c061fcc41fdfd9963b322e
nandan-varma <nandanvarma.me@gmail.com>
2022-12-30 00:22:27 +0530

    minor Changes

--- scripts/data.js
+++ scripts/data.js
@@ -1,19 +1,5 @@
 var updateWindow = document.getElementById("update-window");
 
-// fetch('scripts/tags.json')
-//     .then(function (response){
-//         return response.json();
-//     })
-//     .then(function (tags){
-//         const tagcloud = '.tagcloud';
-//         const options = {
-//             radius : "550",
-//             unit : "%",
-//             keep : false
-//         };
-//         TagCloud(tagcloud, tags, options);
-//     })
-
 fetch('scripts/updates.json')
     .then(function (response) {
         return response.json();
@@ -21,14 +7,13 @@ fetch('scripts/updates.json')
     .then(function (data) {
         data.forEach(x => {
             let box = document.createElement("div");
+            let h2 = document.createElement("h2");
+            let p = document.createElement("p");
             box.className = "box fancy-button";
             box.onclick= function(){
-                window.location.href = "../page.html" + "?id="+ x["id"];
+                window.location.href = "/page.html" + "?id="+ x["id"];
             }
-            // console.log(x["title"])
-            let h2 = document.createElement("h2");
             h2.innerHTML = x["title"];
-            let p = document.createElement("p");
             p.innerHTML = x["content"];
             updateWindow.appendChild(box);
             box.appendChild(h2);
