e15bc1a3a0b076ab57f87a992d3386e553765673
nandan-varma <nandanvarma.me@gmail.com>
2022-12-29 21:51:38 +0530

    manifest and pages (need to clean a bit tho)

---
+++ scripts/article.js
@@ -0,0 +1,20 @@
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+let id = params.id; 
+
+const article = document.getElementById("article");
+const PageTitle = document.getElementById("page-title");
+const PageContent = document.getElementById("page-content");
+
+fetch('pages/'+id.toString(10)+'.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (page) {
+        PageTitle.innerHTML = page["title"];
+        PageContent.innerHTML = page["content"];
+    })
+    .catch (function (err) {
+    console.log('error retrieving data for article ' + err);
+});
--- scripts/data.js
+++ scripts/data.js
@@ -1,5 +1,19 @@
 var updateWindow = document.getElementById("update-window");
 
+// fetch('scripts/tags.json')
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (tags){
+//         const tagcloud = '.tagcloud';
+//         const options = {
+//             radius : "550",
+//             unit : "%",
+//             keep : false
+//         };
+//         TagCloud(tagcloud, tags, options);
+//     })
+
 fetch('scripts/updates.json')
     .then(function (response) {
         return response.json();
@@ -8,6 +22,9 @@ fetch('scripts/updates.json')
         data.forEach(x => {
             let box = document.createElement("div");
             box.className = "box fancy-button";
+            box.onclick= function(){
+                window.location.href = "../page.html" + "?id="+ x["id"];
+            }
             // console.log(x["title"])
             let h2 = document.createElement("h2");
             h2.innerHTML = x["title"];
